ae8961ae20dd0249ed3ff3ba74ee4286f0e56744
Алексей <mr-kaifat@yandex.ru>
2025-06-12 16:32:32 +0300

    KS-Test-Parser completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,21 @@
 package org.example
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+
 fun main() {
-    println("Hello!")
+    val doc: Document = Jsoup.connect(
+        "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-" +
+                "restoran-u-konca-vselennoj/citations/"
+    ).get()
+
+    val quotes: Elements = doc.select("div.sc-2aegk7-2.bzpNIu")
+
+    println("\nЦитаты из 'Автостопом по галактике':\n")
+
+    quotes.forEachIndexed { index, quote ->
+        println("${quote.text()}\n")
+    }
 }
